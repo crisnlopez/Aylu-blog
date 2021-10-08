a6f4e43c96990bde6b89f6eadb66f0b9bb58e812
Cristian Nahuel Lopez <cristianpo@live.com.ar>
2021-10-07 23:05:55 -0300

    logo ig, vsco. Imagen inicial para celular

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
-addEventListener('DOMContentLoaded', () => {
-    const $btn_menu = document.querySelector('#btn-menu')
-    if($btn_menu) {
-        $btn_menu.addEventListener('click', () => {
-            const $nav_list = document.querySelector('.nav-lista')
-            $nav_list.classList.toggle('mostrar') 
-        })
-    }
-})
+// Elementos --Nav--
+const $main = document.querySelector('main')
+const $header = document.querySelector('header')
+const $btn_menu = document.getElementById('btn-menu')
+const $nav_list = document.getElementsByClassName('nav-lista')
+
+// Funciones
+const onOfMenu = () => $nav_list[0].classList.toggle('mostrar')
+const headerSticky = () => $header.classList.toggle('header-sticky')
+
+// llamado de funciones
+$main.addEventListener('scroll', headerSticky())
+$btn_menu.addEventListener('click', onOfMenu)
+
