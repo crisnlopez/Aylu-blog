32ca10210e870bfa5480802eb95db23de4c36e7a
Cristian Nahuel Lopez <cristianpo@live.com.ar>
2021-10-08 19:58:15 -0300

    Transprencia en la versión escritorio del nav. Imagen nuevo de hero-image(responsiva)

--- js/app.js
+++ js/app.js
@@ -2,9 +2,17 @@
 addEventListener('DOMContentLoaded', () => {
     const $btn_menu = document.getElementById('btn-menu');
     const $nav_list = document.getElementsByClassName('nav-lista');
+    const $header = document.querySelector('header')
 
     $btn_menu.addEventListener('click', () => {
         $nav_list[0].classList.toggle('mostrar');
     })
-})
 
+    window.addEventListener('scroll', () => {
+        const scrolled = window.scrollY;
+        
+        (scrolled !== 0) 
+        ? console.info('no estas en top')
+        : console.info('estas en top')
+    })
+})
