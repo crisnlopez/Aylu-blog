cb9380551e81efe487d7be5bde0763a7a721ea75
Cristian Nahuel Lopez <cristianpo@live.com.ar>
2021-10-07 23:29:58 -0300

    Se arreglo boton de menu. Hero-contenedor: 3.5vh

--- js/app.js
+++ js/app.js
@@ -1,14 +1,10 @@
 // Elementos --Nav--
-const $main = document.querySelector('main')
-const $header = document.querySelector('header')
-const $btn_menu = document.getElementById('btn-menu')
-const $nav_list = document.getElementsByClassName('nav-lista')
+addEventListener('DOMContentLoaded', () => {
+    const $btn_menu = document.getElementById('btn-menu');
+    const $nav_list = document.getElementsByClassName('nav-lista');
 
-// Funciones
-const onOfMenu = () => $nav_list[0].classList.toggle('mostrar')
-const headerSticky = () => $header.classList.toggle('header-sticky')
-
-// llamado de funciones
-$main.addEventListener('scroll', headerSticky())
-$btn_menu.addEventListener('click', onOfMenu)
+    $btn_menu.addEventListener('click', () => {
+        $nav_list[0].classList.toggle('mostrar');
+    })
+})
 
