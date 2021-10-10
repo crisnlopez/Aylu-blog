40be0a65ffacabe0bd43f544bc620fc2a76096aa
Cristian Nahuel Lopez <cristianpo@live.com.ar>
2021-10-10 16:11:51 -0300

    navbar flotante implementado en dispositivos con width mayor a a 1029px

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
 // Elementos --Nav--
-addEventListener('DOMContentLoaded', () => {
-    const $btn_menu = document.getElementById('btn-menu');
-    const $nav_list = document.getElementsByClassName('nav-lista');
-    const $header = document.querySelector('header')
+const $btn_menu = document.getElementById('btn-menu');
+const $nav_list = document.getElementsByClassName('nav-lista');
+const $header = document.querySelector('header')
 
+addEventListener('DOMContentLoaded', () => {
     $btn_menu.addEventListener('click', () => {
         $nav_list[0].classList.toggle('mostrar');
     })
+})
 
-    window.addEventListener('scroll', () => {
-        const scrolled = window.scrollY;
-        
-        (scrolled !== 0) 
-        ? console.info('no estas en top')
-        : console.info('estas en top')
-    })
+addEventListener('scroll', () => {
+    const scrolled = window.scrollY;
+    const windowWidth = window.innerWidth;
+
+    if ((windowWidth > 1029) && (scrolled !== 0)) {
+        $header.classList.add('flotante')
+    }  else {
+        $header.classList.remove('flotante')
+    }
 })
