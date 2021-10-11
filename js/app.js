bc0fcba9e5ec7d71314f5a2a0c4435dc23c97654
Cristian Nahuel Lopez <cristianpo@live.com.ar>
2021-10-11 13:54:56 -0300

    Implementación de saludo según la hora

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
 // Elementos --Nav--
-const $btn_menu = document.getElementById('btn-menu');
-const $nav_list = document.getElementsByClassName('nav-lista');
 const $header = document.querySelector('header')
+const $nav_list = document.getElementsByClassName('nav-lista');
+const $btn_menu = document.getElementById('btn-menu');
 
+// Declaración de funciones
+const mostrarSaludo = () => {
+    const fecha = new Date();
+    const hora = fecha.getHours();
+
+    if (hora >= 6 && hora < 12) {
+        document.getElementById('txt-saludo').innerHTML = 'Buenos dias!';
+        document.getElementById('txt-pregunta').innerHTML = 'Qué vas a ver hoy?';
+    }
+    if (hora >= 12 && hora < 20) {
+        document.getElementById('txt-saludo').innerHTML = 'Buenas tardes';
+        document.getElementById('txt-pregunta').innerHTML = 'Qué estás viendo hoy?';
+    }
+    if (hora >= 20 && hora < 6) {
+        document.getElementById('txt-saludo').innerHTML = 'Buenos noches';
+        document.getElementById('txt-pregunta').innerHTML = 'Qué viste hoy?';
+    }
+}
+
+// Eventos
 addEventListener('DOMContentLoaded', () => {
+    mostrarSaludo();
+
     $btn_menu.addEventListener('click', () => {
         $nav_list[0].classList.toggle('mostrar');
     })
