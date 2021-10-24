43ca64c31776149c1e8a30771093e431108d3eed
Cristian Lopez <79811937+crisnlopez@users.noreply.github.com>
2021-10-23 21:14:17 -0300

    formulario contacto, con bootstrap y validación

--- js/contacto.js
+++ js/contacto.js
@@ -1,4 +1,23 @@
-// Elementos --Nav--
+// Validación del formulario
+(function () {
+    'use strict'
+
+    var forms = document.querySelectorAll('.needs-validation')
+
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })();
+
+  // Elementos --Nav--
 const $header = document.querySelector('header')
 const $nav_list = document.getElementsByClassName('nav-lista');
 const $btn_menu = document.getElementById('btn-menu');
